279c9ea07703867e91aebae335483e7bb2d70762
agileca <79025486+agileca@users.noreply.github.com>
2021-02-18 23:58:43 -0500

    Add files via upload

---
+++ wgirls-volunteers.js
@@ -0,0 +1,28 @@
+jQuery(function () {
+    jQuery("#countypane .fl-cta-button").prepend('<div><select id="selcounty" onchange="updateurl(this)"><option value = "">-- Select a region --</option></select></div>');
+
+    jQuery("#countypane .fl-button").css("background-color", "#ddd");
+    jQuery("#countypane .fl-button").css("border-color", "#aaa");
+
+    var selcounty = jQuery("#countypane select");
+    if (selcounty) {
+        for (var i = 0; i < jsondata.length; i++) {
+            var o = new Option(jsondata[i]['county'], jsondata[i]['url']);
+            selcounty.append(o);
+        }                
+    }
+});
+        
+function updateurl(selected) {
+    if (selected.value === "") {
+        console.log("empty");
+        jQuery("#countypane .fl-button").css("background-color", "#ddd");
+        jQuery("#countypane .fl-button").css("border-color", "#aaa");
+        jQuery("#countypane .fl-button").attr("href", "#");
+    }
+    else {
+        jQuery("#countypane .fl-button").css("background-color", "#ed1c24");
+        jQuery("#countypane .fl-button").css("border-color", "#ed1c24");
+        jQuery("#countypane .fl-button").attr("href", selected.value);
+    }
+}
